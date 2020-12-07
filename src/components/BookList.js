182a8e7f95812aeaaba6d222147043077964a392
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-07 20:24:36 +0300

    structure & css

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className='book-list'>
+        <ul>
+          <li>Calıkusu</li>
+          <li>Toprak Ana</li>
+          <li>Gazap Üzümleri</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
