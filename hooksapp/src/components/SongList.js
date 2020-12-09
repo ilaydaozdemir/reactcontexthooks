8292ea3958c425e56a50ad369badd94e1074126b
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2020-12-09 14:29:46 +0300

    useEffect Hook

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NewSongForm from './NewSongForm';
 const { v1: uuidv1 } = require('uuid');
 
@@ -9,9 +9,18 @@ const SongList = () => {
     { title: 'this is darkness', id: 3 },
   ]);
 
+  const [age, setAge] = useState(20);
+
   const addSong = title => {
     setSongs([...songs, { title, id: uuidv1() }]);
   };
+  useEffect(() => {
+    console.log('useEffect hook ran', songs);
+  }, [songs]);
+
+  useEffect(() => {
+    console.log('useEffect hook ran', age);
+  }, [age]);
   return (
     <div className='song-list'>
       <ul>
@@ -20,6 +29,7 @@ const SongList = () => {
         })}
       </ul>
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age:{age}</button>
     </div>
   );
 };
